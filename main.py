f83780f065aad39deb845a178673fb80994a09d4
Malick <malick.hamidou.boureima@gmail.com>
2023-04-26 18:23:40 -0400

    add comments

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import sqlite3
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 
 class Event:
