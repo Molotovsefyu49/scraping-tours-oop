a7ded57df98ade7aee529235948d5a3cdd2fde03
Malick <malick.hamidou.boureima@gmail.com>
2023-04-26 18:22:02 -0400

    add comments

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ class Event:
         return source
 
     def extract(self, source):
+        """Extract relevant data from the page source"""
+        # Uses the selectorlib library to extract data from the page
+        # source based on a YAML file and returns the extracted data.
         extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
         value = extractor.extract(source)["tours"]
         return value
@@ -27,6 +30,9 @@ class Event:
 
 class Email:
     def send(self, message):
+        """Sends an email message"""
+        # Defines parameters for connecting to the SMTP server and
+        # sends an email message using the Smtp  library.
         load_dotenv()
         host = "smtp.gmail.com"
         port = 465
@@ -45,11 +51,15 @@ class Email:
 
 
 class Database:
-
     def __init__(self):
+        """Initializes the database connection"""
+        # Initializes the connection to the SQLite database using the sqlite3 library.
         self.connection = sqlite3.connect("data.db")
 
     def store(self, extracted):
+        """Stores data in the database"""
+        # Splits the extracted data into individual values and
+        # stores them in the database using SQL commands.
         row = extracted.split(',')
         row = [item.strip() for item in row]
         cursor = self.connection.cursor()
@@ -57,11 +67,15 @@ class Database:
         self.connection.commit()
 
     def read(self, extracted):
+        """Reads data from the database"""
+        # Splits the extracted data into individual values and retrieves rows from the database that match those values.
+
         row = extracted.split(',')
         row = [item.strip() for item in row]
         band, city, date = row
         cursor = self.connection.cursor()
-        cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+        cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?",
+                       (band, city, date))
         rows = cursor.fetchall()
         print(rows)
         return rows
@@ -69,11 +83,16 @@ class Database:
 
 if __name__ == "__main__":
     while True:
+        # Creates an Event object, scrapes data from the URL,
+        # extracts relevant data, and prints the extracted data.
         event = Event()
         scraped = event.scrape(URL)
         extracted = event.extract(scraped)
         print(extracted)
 
+        # Checks if the extracted data is "No upcoming tours". If it is not, creates a Database object,
+        # retrieves rows from the database that match the extracted data, and if no matches are found,
+        # stores the extracted data in the database and sends an email.
         if extracted != "No upcoming tours":
             database = Database()
             row = database.read(extracted)
